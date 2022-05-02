b9bcea79f777bffe5c375950271a694a27cbc28b
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-02 05:19:18 +0000

    feature-3-fav-and-skip local storage added

--- js/data.js
+++ js/data.js
@@ -5,3 +5,16 @@ var data = {
   // skip: [],
   list: []
 };
+
+var previousDataListJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousDataListJSON !== null) {
+  data.list = JSON.parse(previousDataListJSON);
+}
+
+function stringifyFunction(event) {
+  var dataListJSON = JSON.stringify(data.list);
+  localStorage.setItem('javascript-local-storage', dataListJSON);
+}
+
+window.addEventListener('beforeunload', stringifyFunction);
